f94ac600a13dea4492f8f414e903a82f00367997
murcafes94 <esaumuri94@gmail.com>
2026-08-24 09:45:00 -0500

    feat: integrar sesiones de No molestar en Activities

--- recovered-v3/app/src/main/java/com/fabri/ministerium/ThemedActivity.java
+++ recovered-v3/app/src/main/java/com/fabri/ministerium/ThemedActivity.java
@@ -4,8 +4,34 @@ import android.app.Activity;
 import android.content.Context;
 
 public abstract class ThemedActivity extends Activity {
+    private boolean prayerFocusEntered;
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(ThemeUtils.wrap(newBase));
+    }
+
+    /** Las pantallas litúrgicas sobrescriben este método para activar No molestar. */
+    protected boolean usesPrayerFocus() {
+        return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (usesPrayerFocus()) {
+            PrayerFocusController.enter(this);
+            prayerFocusEntered = PrayerFocusController.isEnabled(this)
+                    && PrayerFocusController.hasPolicyAccess(this);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if (prayerFocusEntered) {
+            PrayerFocusController.exit(this);
+            prayerFocusEntered = false;
+        }
+        super.onStop();
     }
 }
